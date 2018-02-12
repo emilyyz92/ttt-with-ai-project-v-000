7075fc191521a3d2137effd0eb20c732a920f85d
Emily Zhao <tong.zhao.92@gmail.com>
2018-02-12 19:16:29 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -58,8 +58,8 @@ module Players
         end
         if c_cmb != nil #first check if computer has winning chance
            c_win = c_cmb.detect do |c| #check if computer can win (is there a winning combo occupied that has vacant cells?)
-             binding.pry
              c.detect do |c1|
+               binding.pry
                c2 = c1 + 1
                !board.taken?(c2.to_s)
              end
